872e9e9bfcf52da725d801ea0f95cd245e6106fb
JobYegor <alohawind@mail.ru>
2016-09-19 20:27:14 +0300

    -a
    first commit

---
+++ data.php
@@ -0,0 +1,6 @@
+<?
+if( empty($_POST['data']) ) echo 'Все плохо';
+	echo 'Все хорошо!!!!!';
+	// $data = json_encode($_POST['data']);
+	print ($_POST['data']);
+?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="data.php" method="POST" class="forma">
+	<div class="text1"><span class="firstname">Имя: </span><input type="text" class="field1"></div>
+	<div class="text2"><span class="secondname">Фамилия: </span><input type="text" class="field2"></div>
+	<input type="submit" class="button" value="Отправить">
+</form>
+<div class="result"></div>
+<script src="js/jquery-2.1.4.js"></script>
+<script>
+	$(document).ready(function() {
+		$('.button').click(function() {
+
+		var dataform = {
+				firstname : $('.field1').val(),
+				secondname : $('.field2').val()
+			};
+
+		var data = JSON.stringify(dataform);
+		
+		$.ajax({
+			url: 'data.php',
+			type: 'post',
+			data: {
+				data: data
+			},
+			success: function(data) {
+				$('.result').html('data');
+			}
+		});
+		return false
+		});
+	})
+</script>
+</body>
+</html>
